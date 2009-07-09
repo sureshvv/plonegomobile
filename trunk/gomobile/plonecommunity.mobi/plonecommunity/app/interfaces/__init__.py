0b1d7431b1422c76fda19ac814b3016c560f532e
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2009-07-09 13:30:44 +0000

    Added mobile analytics tracking code.
    Updated portal_actions -> hid print.
    
    Updated feed item and feed folder listings.

--- trunk/gomobile/plonecommunity.mobi/plonecommunity/app/interfaces/__init__.py
+++ trunk/gomobile/plonecommunity.mobi/plonecommunity/app/interfaces/__init__.py
@@ -3,6 +3,9 @@ import zope.interface
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope.viewlet.interfaces import IViewletManager
 
+
+from gomobile.mobile.interfaces import IMobileLayer
+
 class IThemeLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     
