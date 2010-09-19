ae0ef878718854d1f6776b8fdd3e381a9a37ee92
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-09-19 05:02:00 +0000

    iPhone4 tweak

--- trunk/gomobile/gomobiletheme.basic/gomobiletheme/basic/static/mobilethemehelper.js
+++ trunk/gomobile/gomobiletheme.basic/gomobiletheme/basic/static/mobilethemehelper.js
@@ -7,7 +7,33 @@
  * 
  */
 function fixActionImages() {
-        var width = jQuery(window).width();
+	
+	var multiplier;
+	if(window.devicePixelRatio) {
+		// iPhone 4
+		
+		multiplier = window.devicePixelRatio;
+	} else {
+		multiplier = 1;
+	}
+
+        if(multiplier == 2) {
+	
+	    // Compensate iPhone 4's 2x reported scale with 0.5x zoom
+	    // jQuery seemed to have issue manipulating meta tags
+	    document.getElementById("viewport").setAttribute("content", "width=device-width; initial-scale=0.5; minimum-scale=0.5; maximum-scale=0.5; user-scalable=no;");
+		
+	    // Load our special stylesheet having text size fixes for iPhone 4
+	    jQuery("head").append("<link>");
+	    css = jQuery("head").children(":last");
+	    css.attr({
+	      rel:  "stylesheet",
+	      type: "text/css",
+	      href: "++resource++gomobiletheme.basic/iphone4.css"
+	    });
+	}
+
+        var width = jQuery(window).width() * multiplier;
 
 	if(width > 480) {
 	       // Assume smartphone with resolution in 640 x 480 range
