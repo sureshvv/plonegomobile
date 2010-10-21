c8aea8a164a3e08055fb68c5b010d79d4cdc93e3
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-10-21 08:29:13 +0000

    Tagged mobile.heurestics 0.9.

---
+++ mobile.heurestics/tags/0.9/mobile/heurestics/sniffed.py
@@ -0,0 +1,13 @@
+"""
+
+    Simple heurestics rules.
+
+"""
+
+__author__ = "Mikko Ohtamaa <mikko.ohtamaa@twinapex.com>"
+__copyright__ = "2010 Twinapex Research"
+__license__ = "GPL"
+__docformat__ = "epytext"
+
+
+# Nothing here yet
