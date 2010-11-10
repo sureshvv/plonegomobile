063e556dceff4a8d982895fd9f92cb1dead8b70d
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-11-10 11:46:00 +0000

    Tagged gomobiletheme.basic 0.9.2.

---
+++ gomobiletheme.basic/tags/0.9.2/gomobiletheme/basic/__init__.py
@@ -0,0 +1,7 @@
+from zope.i18nmessageid import MessageFactory
+
+# your.app.package must match domain declaration in .po files
+MessageFactory = MessageFactory('gomobiletheme.basic')
+
+def initialize(context):
+    pass
