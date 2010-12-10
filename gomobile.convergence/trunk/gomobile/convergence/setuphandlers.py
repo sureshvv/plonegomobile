1364dc4757adf608db5672e8c439f07f41794b0f
maurits.vanrees <maurits.vanrees@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-12-10 11:24:17 +0000

    Added flag file so our special GenericSetup import step is only run when applying our own profile.  This avoids lots of reindexing.

--- gomobile.convergence/trunk/gomobile/convergence/setuphandlers.py
+++ gomobile.convergence/trunk/gomobile/convergence/setuphandlers.py
@@ -1,12 +1,17 @@
 __license__ = "GPL 2.1"
 __copyright__ = "2009 Twinapex Research"
 
+
 def importFinalSteps(context):
     """
     The last bit of code that runs as part of this setup profile.
     """
+    # Only run step if a flag file is present, otherwise this step
+    # also gets run when applying a totally unrelated profile.
+    if context.readDataFile('gomobile_convergence-default.txt') is None:
+        return
     site = context.getSite()
-    
+
     # GenericProfiles nukes catalog indexes on each quick installer run
     # work around this "feature" by reindexing the whole site
     # after the quick installer has been run
