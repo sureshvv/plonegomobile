3a4ff0aeba894dae2ad274b590f004715f246ad6
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2011-03-14 14:15:13 +0000

    Tagged gomobile.templates 1.0.2.

---
+++ gomobile.templates/tags/1.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.2'
+
+setup(name='gomobile.templates',
+      version=version,
+      description="Project templates creating Web and Mobile themes for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='skeleton web mobile templates plone apps',
+      author='mFabrik Research Oy',
+      author_email='research@mfabrik.com',
+      url='http://webandmobile.mfabrik.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gomobile'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'PasteScript',
+          'ZopeSkel',
+          # -*- Extra requirements: -*-
+      ],      entry_points="""
+      # -*- Entry points: -*-
+      [paste.paster_create_template]
+      gomobile_theme = gomobile.templates.theme:Theme  
+        
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
