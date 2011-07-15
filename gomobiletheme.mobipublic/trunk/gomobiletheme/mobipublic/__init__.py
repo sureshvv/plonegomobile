8e96b5fd4339c4bdb954a7e21fc813161a0111fd
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2011-07-15 17:11:33 +0000

    Initial front page block code

--- gomobiletheme.mobipublic/trunk/gomobiletheme/mobipublic/__init__.py
+++ gomobiletheme.mobipublic/trunk/gomobiletheme/mobipublic/__init__.py
@@ -1,7 +1,9 @@
 from zope.i18nmessageid import MessageFactory
 
+
 # Declare the function which will turn strings to translation ids
 MessageFactory = MessageFactory('${namespace_package}.${package}')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
