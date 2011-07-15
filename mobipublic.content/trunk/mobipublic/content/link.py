8e4edf3e68cd42242459f0029997aa43c7acb213
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2011-07-15 10:30:17 +0000

    Added Deal & Link content type

---
+++ mobipublic.content/trunk/mobipublic/content/link.py
@@ -0,0 +1,57 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+
+from z3c.form import group, field
+from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
+
+from mobipublic.content import MessageFactory as _
+
+from plone.namedfile.field import NamedImage
+
+# Interface class; used to define content-type schema.
+
+class ILink(form.Schema):
+    """
+    Directory entry
+    """
+    
+    remoteUrl = schema.URI(title=u"URL", required=True)
+
+    screenshot = NamedImage(title=u"Screenshot", 
+                               description=u"Will be automatically resized", 
+                               required=False)
+
+
+class Link(dexterity.Item):
+    grok.implements(ILink)
+    
+    # Add your class methods and properties here
+
+
+# View class
+# The view will automatically use a similarly named template in
+# link_templates.
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@sampleview" appended.
+# You may make this the default view for content objects
+# of this type by uncommenting the grok.name line below or by
+# changing the view class name and template filename to View / view.pt.
+
+class View(grok.View):
+    grok.context(ILink)
+    grok.require('zope2.View')
+    grok.name("view")
+    
+    def hasImage(self):
+        """
+        """
+        return self.context.screenshot != None
+    
+    def getImageURL(self):
+        """
+        """
+        return 
+    # grok.name('view')
