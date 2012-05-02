0518926776d7917e49c9670884e0ff0ed7fa02d4
miohtama@gmail.com <miohtama@gmail.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2012-05-02 10:55:39 +0000

    Prepare gomobile.convergence 1.0.2.

--- gomobile.convergence/trunk/gomobile/convergence/Extensions/Install.py
+++ gomobile.convergence/trunk/gomobile/convergence/Extensions/Install.py
@@ -17,13 +17,24 @@ def uninstall(portal, reinstall=False):
     output = StringIO()
     if not reinstall:
 
+        # Clean up generic multichannel per document settings
         try:
             report = clean_up_content_annotations(portal, ["multichannel"])
         except Exception, e:
             print >> str(e), output
-        
+
         print >> output, report
-        
+        #print report        
+            
+        # Clean up per-document convergence field overrrides
+        try:
+            report = clean_up_content_annotations(portal, ["mobile_override_values"])
+        except Exception, e:
+            print >> str(e), output
+
+        #print report
+    
+        print >> output, report    
         print >> output, "Ran all uninstall steps."
         
         return output.getvalue()
