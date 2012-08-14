fbf322261b42c2ff57206ec515f6d1f962e79999
miohtama <miohtama@985c90ce-6bbf-11de-a1e0-47924000d34a>
2012-08-14 13:25:15 +0000

    Tagging 1.0.8

---
+++ gomobiletheme.basic/tags/1.0.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.8'
+
+setup(name='gomobiletheme.basic',
+      version=version,
+      description="Default mobile theme for mFabrik Web and Mobile sites",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='mFabrik Research Oy',
+      author_email='research@mfabrik.com',
+      url='http://webandmobile.mfabrik.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gomobiletheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'five.grok',
+          'collective.fastview',
+          'gomobile.mobile'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
